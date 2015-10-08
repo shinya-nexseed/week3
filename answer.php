ad426d8fe26b74f029de862641dcff87ab5b75e0
shinyahirai <908.shinya@gmail.com>
2015-10-08 11:58:23 +0800

    week3 day4

---
+++ answer.php
@@ -0,0 +1,44 @@
+<?php
+    // var_dump($_POST);
+    $nickname = $_POST["nickname"];
+    $comment = $_POST["comment"];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>アンサー</title>
+</head>
+<body>
+  <h1>掲示版</h1>
+  <ul>
+    <li><?php echo $nickname; ?> : <?php echo $comment; ?></li>
+  </ul>
+  <a href="post_sample.php">つぶやき画面へ</a>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
